93cb5d331d990b78643ffeb8d08a63209245035d
Rajbir Shaw <shawrajbir78@gmail.com>
2026-07-29 21:05:32 +0530

    Time: 2 ms (58.66%), Space: 9.3 MB (38.84%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // DP table initialization
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        // Base case: Empty pattern can match empty string
+        dp[0][0] = true;
+        
+        // Fill in the DP table for patterns that can match an empty string
+        for (int j = 1; j <= n; j++) {
+            if (p[j-1] == '*') {
+                dp[0][j] = dp[0][j-2];
+            }
+        }
+
+        // Fill the DP table for the rest of the string and pattern
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j-1] == s[i-1] || p[j-1] == '.') {
+                    dp[i][j] = dp[i-1][j-1];
+                } 
+                else if (p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-2] || (p[j-2] == s[i-1] || p[j-2] == '.') && dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
